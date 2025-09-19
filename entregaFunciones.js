4ca3c1bc2d39fdb8c67bee4e33b85aeb57adf965
chamboyas7-gif <chamboyas7@gmail.com>
2025-09-19 09:59:20 -0600

    Add files via upload

---
+++ entregaFunciones.js
@@ -0,0 +1,14 @@
+function mult(n1, n2){
+    return n1 * n2;
+}
+const multi=(n1, n2)=>{
+    return n1 * n2;
+}
+
+function cuadr(n1){
+    return n1 * n1;
+}
+const cuadra=(n1)=>{
+    return n1 * n1;
+}
+
